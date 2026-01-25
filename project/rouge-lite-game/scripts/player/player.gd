12a5a9593adbb76c095e073c60aa63739638fff0
Lennox <lennoxmurg@gmail.com>
2026-01-25 13:57:00 +0100

    Player code getestet und überarbeitet
    der Code für das Player Movement eingefügt, Überarbeitet

--- project/rouge-lite-game/scripts/player/player.gd
+++ project/rouge-lite-game/scripts/player/player.gd
@@ -1,17 +1,14 @@
-extends CharacterBody2D
+class_name Player extends CharacterBody2D
 
-const SPEED : float = 100.0
+var move_speed : float = 75.0
+
 
 
 func _physics_process(delta: float) -> void:
-	var input_vector := Vector2.ZERO
+	var direction : Vector2 = Vector2.ZERO
+	direction.x = Input.get_action_strength("move_right") - Input.get_action_strength("move_left")
+	direction.y = Input.get_action_strength("move_down") - Input.get_action_strength("move_up")
 	
-	input_vector.x = Input.get_action_strength("move_right") - Input.get_action_strength("move_left")
-	input_vector.y = Input.get_action_strength("move_down") - Input.get_action_strength("move_up")
-	
-	input_vector = input_vector.normalized()
-	
-	velocity = input_vector * SPEED
-	
+	velocity = direction * move_speed
 	
 	move_and_slide()
