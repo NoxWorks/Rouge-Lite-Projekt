318dba7430909786300d20ecb84b14722583e71f
Lennox <lennoxmurg@gmail.com>
2026-01-25 19:07:22 +0100

    Kollisionen, Platzhalter und neue .md
    Simple Collision Layer mit namen Hinzugefügt, Tisch Platzhalter Sprite zum Visualisieren der Kollisionen Hinzugefügt, kollisionen.md als Dokumentation für die Unterschiedlichen Kollisionen und Layer/Masks hinzugefügt

---
+++ project/docs/kollisionen.md
@@ -0,0 +1,10 @@
+# Collision Layer Übersicht
+
+| Layer | Name        | Beschreibung                     |
+|-------|-------------|----------------------------------|
+| 1     | Player      | Spielercharaktere                |
+| 2     | Furniture   | Möbel, Deko                      |
+| 3     | Enemy       | Gegner                           |
+| 4     | Wall        | Wände, Hindernisse               |
+| 5     | Door        | Türen, Übergänge                 |
+| 6     | Switch      | Buttons, Schalter                |
--- project/rouge-lite-game/scripts/player/player.gd
+++ project/rouge-lite-game/scripts/player/player.gd
@@ -1,14 +1,23 @@
 class_name Player extends CharacterBody2D
 
+@export var debug : bool = false
+
 var move_speed : float = 75.0
 
 
 
-func _physics_process(delta: float) -> void:
+func _physics_process(_delta: float) -> void:
 	var direction : Vector2 = Vector2.ZERO
 	direction.x = Input.get_action_strength("move_right") - Input.get_action_strength("move_left")
 	direction.y = Input.get_action_strength("move_down") - Input.get_action_strength("move_up")
 	
 	velocity = direction * move_speed
+
+	if debug:
+		debugging()
+
 	
 	move_and_slide()
+
+func debugging() -> void:
+	print(position)
