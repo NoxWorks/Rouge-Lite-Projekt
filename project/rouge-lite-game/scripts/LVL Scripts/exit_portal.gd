d0fc7ec05689931c93aaac79c6deb1da7d120154
Lennox <lennoxmurg@gmail.com>
2026-01-28 20:31:30 +0100

    Formatierung, Gegner Sprite
    doc formatierung und richtige transparenz in den Gegner sprite eingefügt

--- project/rouge-lite-game/scripts/LVL Scripts/exit_portal.gd
+++ project/rouge-lite-game/scripts/LVL Scripts/exit_portal.gd
@@ -1,7 +1,7 @@
 extends Node2D
 
-# If this bool is true the level will end
-# If this bool is false it will restart the level
+#  if true, it ends the game
+# if false, the portal restarts the level
 @export var end_game : bool = false
 
 
@@ -9,7 +9,7 @@ func _on_area_2d_body_entered(body: Node2D) -> void:
 	if end_game:
 		if body.name == "Player": # Ends the game
 			call_deferred("quit_game")
-	else: # Restarts the level
+	elif end_game == false: # Restarts the level
 		call_deferred("reload_level")
 
 func quit_game():
